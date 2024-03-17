b37e5d5a29a94a391586d3db00a3dfdf167c6ee8
Jessica Prestelo <jessicaprestelotds2senai@gmail.com>
2024-03-17 15:23:33 -0300

    Métodojoin

---
+++ atividadearray.js
@@ -0,0 +1,13 @@
+//.join()
+//O método join() é usado para criar uma string a partir dos elementos de um array
+// O método join() junta os elementos do array em uma única string
+/// separando cada elemento com um separador especificado.
+//EX1:
+const frutas = ["maçã", "banana", "laranja"];
+const resultado = frutas.join(", ");
+console.log(resultado); // Output: maçã, banana, laranja
+
+//EX2:
+const animais = ["macaco", "zebra", "abelha"];
+const resultado = animais.join(", ");
+console.log(resultado); // Output: macaco,  zebra, abelha
